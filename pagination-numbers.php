6d5aba853a815e177c838b5f5b0fe78b8b39acac
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2022-09-23 14:52:10 +0700

    Update pagination-numbers.php

--- pagination-numbers.php
+++ pagination-numbers.php
@@ -1,31 +1,36 @@
 <?php
 
-function neon_pagination($query =null ) {
-    if ( empty($query) ) {
-        global $wp_query;
-        $query = $wp_query;
-    }
+if ( ! function_exists('neon_pagination') ) {
+	function neon_pagination($query =null ) {
+		if ( empty($query) ) {
+			global $wp_query;
+			$query = $wp_query;
+		}
 
-    $total = $query->max_num_pages;
+		$total = $query->max_num_pages;
 
-    if ( $total > 1 )  {
-        $current_page = get_query_var('paged') ? : 1;
-      
-        
+		if ( $total > 1 )  {
+			$is_static_page = get_query_var('page');
+			if ( empty($is_static_page) ) {
+				$current_page = get_query_var('paged') ? : 1;
+			} else {
+				$current_page = $is_static_page;
+			}
 
-        // Structure of “format” depends on whether we’re using pretty permalinks
-        $format = empty( get_option('permalink_structure') ) ? '&page=%#%' : 'page/%#%/';
-        echo paginate_links(array(
-            'base' => get_pagenum_link(1) . '%_%',
-            'format' => $format,
-            'current' => $current_page,
-            'total' => $total,
-            'prev_text'          => apply_filters('neon_prev_nav', '&laquo; Sebelumnya'),
-            'next_text'          => apply_filters('neon_next_nav', 'Berikutnya &raquo;'),
-            'mid_size' => 4,
-            'type' => 'list'
-        ));
-    }
+			// Structure of “format” depends on whether we’re using pretty permalinks
+			$format = empty( get_option('permalink_structure') ) ? '&page=%#%' : 'page/%#%/';
+			echo paginate_links(array(
+				'base' => get_pagenum_link(1) . '%_%',
+				'format' => $format,
+				'current' => $current_page,
+				'total' => $total,
+				'prev_text'          => apply_filters('neon_prev_nav', '&laquo; Sebelumnya'),
+				'next_text'          => apply_filters('neon_next_nav', 'Berikutnya &raquo;'),
+				'mid_size' => 4,
+				'type' => 'list'
+			));
+		}
+	}
 }
 
 # user
