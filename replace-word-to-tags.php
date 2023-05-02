aed487ba59dd6c19a9f7ca9f1ac4e60f27724b50
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2023-05-02 23:48:59 +0700

    Create replace-word-to-tags.php

---
+++ replace-word-to-tags.php
@@ -0,0 +1,15 @@
+<?php
+
+add_filter('the_content', 'addLinkToTags', 0);
+function addLinkToTags($content) {
+    $post_tags = get_the_tags();
+    if ($post_tags) {
+        foreach($post_tags as $tag) {
+            $replacement = sprintf('<a href="%s">%s</a>', get_term_link($tag), $tag->name);
+            $target = $tag->name;
+            $content = preg_replace('/(<p>.*?)(\b' . $target . '\b)(.*?<\/p>)/i', '$1' . $replacement . '$3', $content, 1);
+        }
+    }
+
+    return $content;
+}
