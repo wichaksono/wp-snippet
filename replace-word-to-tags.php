55a541545e638943dec01539eb33600aea39f5c3
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2023-05-08 09:52:52 +0700

    Update replace-word-to-tags.php
    only for single post

--- replace-word-to-tags.php
+++ replace-word-to-tags.php
@@ -3,7 +3,7 @@
 add_filter('the_content', 'addLinkToTags', 0);
 function addLinkToTags($content) {
     $post_tags = get_the_tags();
-    if ($post_tags) {
+    if ( $post_tags && is_single() ) {
         foreach($post_tags as $tag) {
             $replacement = sprintf('<a href="%s">%s</a>', get_term_link($tag), $tag->name);
             $target = $tag->name;
