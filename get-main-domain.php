321b9dd5476393f1248583a2aca411cb193adbe4
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2024-03-09 09:57:09 +0700

    Update get-main-domain.php

--- get-main-domain.php
+++ get-main-domain.php
@@ -16,3 +16,17 @@ function getMainDomain() {
 
 	return str_replace($subdir, '', $request_uri);
 }
+
+
+function getBaseUrl() {
+	$subDirectoryName = $_SERVER['SCRIPT_FILENAME'];
+        $subDirectoryName = str_replace($_SERVER['DOCUMENT_ROOT'],  '', $subDirectoryName);
+        $subDirectoryName = str_replace('/index.php',  '', $subDirectoryName);
+
+        $protocol = isset($_SERVER['HTTPS']) && ($_SERVER['SERVER_PORT'] == 443 || $_SERVER['HTTPS'] == 'on')
+            ? 'https' : 'http';
+
+        $baseUrl = $protocol . '://' . $_SERVER['HTTP_HOST'] . $subDirectoryName;
+
+        echo $baseUrl;
+}
