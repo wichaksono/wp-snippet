118deeea396342568687b3ab9164c242e0fef391
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2024-12-03 13:29:01 +0700

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,12 @@
+<?php
+
+// limit revision
+define('WP_POST_REVISIONS', 3);
+
+// force https
+if (
+  (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') 
+  || (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+){
+    $_SERVER['HTTPS'] = 'on';
+}
