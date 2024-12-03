d3df8c2464ea083cc78329af778666acf49be9cf
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2024-12-03 13:50:24 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -5,8 +5,8 @@ define('WP_POST_REVISIONS', 3);
 
 // force https
 if (
-  (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') 
-  || (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
-){
+    (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') 
+    && (empty($_SERVER['HTTP_X_FORWARDED_PROTO']) || $_SERVER['HTTP_X_FORWARDED_PROTO'] !== 'https')
+) {
     $_SERVER['HTTPS'] = 'on';
 }
